47fbff64d1b18614c909ab458a0038c514544f2e
Russell Bega <russell.alexandre@gmail.com>
2020-04-18 19:21:52 -0300

    Add likes count test on repo update
    https://github.com/Rocketseat/bootcamp-gostack-desafios/issues/17

--- src/__tests__/likes.spec.js
+++ src/__tests__/likes.spec.js
@@ -33,4 +33,42 @@ describe("Likes", () => {
       .post(`/repositories/123/like`)
       .expect(400);
   });
+
+  it("should not lose current likes when update repository info", async () => {
+    const repository = await request(app)
+      .post("/repositories")
+      .send({
+        url: "https://github.com/Rocketseat/umbriel",
+        title: "Umbriel",
+        techs: ["Node", "Express", "TypeScript"]
+      });
+
+    let response = await request(app).post(
+      `/repositories/${repository.body.id}/like`
+    );
+
+    expect(response.body).toMatchObject({
+      likes: 1
+    });
+
+    response = await request(app).post(
+      `/repositories/${repository.body.id}/like`
+    );
+
+    expect(response.body).toMatchObject({
+      likes: 2
+    });
+
+    response = await request(app)
+      .put(`/repositories/${repository.body.id}`)
+      .send({
+        url: "https://github.com/Rocketseat/unform",
+        title: "Unform",
+        techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
+      });
+
+    expect(response.body).toMatchObject({
+      likes: 2
+    });
+  });
 });
