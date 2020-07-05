0bb25783118cc3e412c65cc85597471f607b3584
Ruanderson Vieira <ruanderson@accalia.io>
2020-07-05 19:11:36 -0300

    :sparkles: list repositories

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
